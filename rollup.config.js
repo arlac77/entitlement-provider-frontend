16094d44c0fe023b8f9c4c2dfc6c4945089f4a46
Markus Felten <Markus.Felten@gmx.de>
2019-12-02 02:01:14 +0100

    chore(package): rollup@^1.27.7 chore(rollup): import config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import { config } from "./package.json";
+
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
