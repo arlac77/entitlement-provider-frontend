ab521daf69ab0280a2b9f4dd78dec683586bf619
Markus Felten <Markus.Felten@gmx.de>
2019-12-05 18:40:24 +0100

    fix(rollup): use @rollup/plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import svelte from "rollup-plugin-svelte";
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 import consts from 'rollup-plugin-consts';
