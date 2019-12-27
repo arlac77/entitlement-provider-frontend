d9d8345ca5e12934af2fecdf4c4ce02838f0805f
Markus Felten <Markus.Felten@gmx.de>
2019-12-27 01:34:24 +0100

    fix: endpoint cirlces

--- src/pages/Services.svelte
+++ src/pages/Services.svelte
@@ -2,8 +2,8 @@
   import { session } from "../main.mjs";
   import api from "consts:api";
 
-  const sh = 40;
-  const sw = 140;
+  const sh = 50;
+  const sw = 100;
 
   async function fetchServices() {
     return fetch(`${api}/services`, {
@@ -15,42 +15,70 @@
         for (const service of Object.values(json)) {
           service.x = 10;
           service.y = y;
-          y += sh;
+
+          let ey = 10 + 5;
+          for (const endpoint of Object.values(service.endpoints)) {
+            endpoint.x = sw - 10;
+            endpoint.y = ey;
+            if(endpoint.connected === undefined) {endpoint.connected = [];}
+            ey += 12;
+          }
+
+          service.w = sw;
+          service.h = ey > sh ? ey : sh;
+
+          y += service.h + 10;
         }
 
         return json;
       });
   }
 
-  let i = 0;
+  let width = 400;
+  let height = 700;
 
-  let width = 500;
-  let height = 800;
+  function endpointFor(services,exp) {
+    const m = exp.match(/service\((\w+)\)\.(\w+)/);
+
+    if(m) {
+      return services[m[1]].endpoints[m[2]];
+    }
+  }
 </script>
 
 <style>
   svg {
-    width: 100%;
-    height: 100%;
+    width: 33%;
+    height: 33%;
   }
 
-  .services rect {
-    fill: rgb(35, 170, 17);
+  .service rect {
     stroke: none;
-    opacity: 0.65;
+    opacity: 0.35;
   }
 </style>
 
 {#await fetchServices()}
   <p>...fetching</p>
 {:then services}
-  <svg>
+  <svg viewbox="0 0 {width} {height}">
     <g class="services">
       {#each Object.values(services) as service}
-        <rect x={service.x} y={service.y} width={sw} height={sh - 10} />
-        <text x={service.x + 5} y={service.y + 18}>
-          {service.name} ({service.state})
-        </text>
+        <g class="service" transform="translate({service.x},{service.y})">
+          <rect
+            x="0"
+            y="0"
+            width={service.w}
+            height={service.h}
+            fill={service.state === 'running' ? 'green' : 'red'} />
+          <text x="8" y="22">{service.name}</text>
+          {#each Object.values(service.endpoints) as endpoint}
+            <circle cx={endpoint.x} cy={endpoint.y} r="5" />
+            <!--{#each endpoint.connected as connected}
+              <path d="C {endpoint.x} {endpoint.y} 0 0 50 50" stroke="blue" fill="transparent"/>
+            {/each}-->
+          {/each}
+        </g>
       {/each}
     </g>
   </svg>
