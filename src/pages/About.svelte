b89fd720a51fa04343588e36c81f8556c8b3114f
Markus Felten <Markus.Felten@gmx.de>
2020-09-17 18:13:23 +0200

    fix: make use of ServerDetails

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,46 +1,30 @@
 <script>
   import {
-    DateTime,
-    Duration,
-    formatBytes,
     About,
-    SessionDetails
+    SessionDetails,
+    ServerDetails
   } from "svelte-common";
   import { websocketStore } from "svelte-websocket-store";
   import { session } from "../main.mjs";
+  import name from "consts:name";
   import version from "consts:version";
   import description from "consts:description";
-  import name from "consts:name";
   import api_ws from "consts:api_ws";
 
   const protocols = ["access_token", session.access_token];
 
   let uptime = websocketStore(api_ws + "/state/uptime", -1, protocols);
-  let memory = websocketStore(
-    api_ws + "/state/memory",
-    { heapTotal: -1, heapUsed: -1, external: -1, rss: -1 },
-    protocols
-  );
+  let memory = websocketStore(api_ws + "/state/memory", {}, protocols);
+
+  let server = {};
+
+  $: {
+    server.uptime = $uptime;
+    server.memory = $memory;
+  }
 </script>
 
 <About {name} {version} {description}>
-  <tr>
-    <td>Uptime</td>
-    <td>
-      {#if $uptime < 0}
-        down
-      {:else}
-        <Duration seconds={$uptime} />
-      {/if}
-    </td>
-  </tr>
-  <tr>
-    <td>Server Heap Total</td>
-    <td>{formatBytes($memory.heapTotal)}</td>
-  </tr>
-  <tr>
-    <td>Server Heap Used</td>
-    <td>{formatBytes($memory.heapUsed)}</td>
-  </tr>
+  <ServerDetails {server} />
   <SessionDetails session={$session} />
 </About>
