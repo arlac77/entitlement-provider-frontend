704d569b780137c66bd9ac4ec186d1d0471221dd
Markus Felten <Markus.Felten@gmx.de>
2020-09-27 01:21:15 +0200

    fix: use title for About

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -9,7 +9,7 @@
   } from "svelte-common";
   import { websocketStore } from "svelte-websocket-store";
   import { session, serviceWorker, serviceWorkerRegistration } from "../main.mjs";
-  import name from "consts:name";
+  import name from "consts:title";
   import version from "consts:version";
   import description from "consts:description";
   import api_ws from "consts:api_ws";
