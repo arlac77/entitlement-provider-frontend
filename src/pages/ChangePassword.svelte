93d8a37f96f7dedbb42cdc89a0f57965b7e4fb7d
Markus Felten <Markus.Felten@gmx.de>
2020-10-06 20:47:09 +0200

    fix: simple change password dialog

--- src/pages/ChangePassword.svelte
+++ src/pages/ChangePassword.svelte
@@ -1,12 +1,87 @@
 <script>
-  import { Login } from "svelte-session-manager";
   import { Modal } from "svelte-common";
   import { session } from "../main.mjs";
   import api from "consts:api";
 
   export let router;
+
+  let username = "";
+  let password = "";
+
+  let active = false;
+  let message;
+
+  async function submit() {
+    try {
+      active = true;
+      /*message = await login(session, endpoint, username, password);
+      if(!message && result !== undefined) {
+        await result();
+      }*/
+    } catch (e) {
+      message = e;
+    } finally {
+      active = false;
+      password = "";
+    }
+  }
 </script>
 
 <Modal close={() => router.abort()}>
-  <h3>Change Password</h3>
+
+<form on:submit|preventDefault={submit}>
+  {#if message}
+    <slot name="message">
+      <div class="error" id="message">{message}</div>
+    </slot>
+  {/if}
+
+  <slot name="inputs">
+    <label for="username">
+      Username
+      <input
+        aria-label="username"
+        aria-required="true"
+        maxlength="75"
+        size="32"
+        autocorrect="off"
+        autocapitalize="off"
+        autocomplete="username"
+        id="username"
+        type="text"
+        placeholder="Username"
+        name="username"
+        required
+        disabled={active}
+        bind:value={username} />
+    </label>
+    <label for="password">
+      Password
+      <input
+        aria-label="password"
+        aria-required="true"
+        size="32"
+        autocorrect="off"
+        autocapitalize="off"
+        autocomplete="current-password"
+        id="password"
+        type="password"
+        placeholder="Password"
+        name="password"
+        required
+        disabled={active}
+        bind:value={password} />
+    </label>
+  </slot>
+
+  <slot name="submit">
+    <button id="submit" type="submit" disabled={!username || !password}>
+      Change Password
+      {#if active}
+        <div class="spinner" />
+      {/if}
+    </button>
+  </slot>
+</form>
+
 </Modal>
