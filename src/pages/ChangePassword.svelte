237132a3ebf4bcc570c469e2005ca50e7c3a751d
Markus Felten <Markus.Felten@gmx.de>
2020-11-28 04:10:43 +0100

    fix: add user to password change call

--- src/pages/ChangePassword.svelte
+++ src/pages/ChangePassword.svelte
@@ -6,6 +6,7 @@
   export let router;
 
   let username = "";
+  let password = "";
   let newPassword = "";
   let repeatedNewPassword = "";
 
@@ -15,11 +16,15 @@
   async function submit() {
     try {
       active = true;
-      
+
       const response = await fetch(`${api}/password`, {
-         method: 'PATCH',
-         headers: session.authorizationHeader,
-         body: JSON.stringify({password: newPassword })
+        method: "PATCH",
+        headers: session.authorizationHeader,
+        body: JSON.stringify({
+          user: username,
+          password,
+          new_password: newPassword
+        })
       });
     } catch (e) {
       message = e;
@@ -31,78 +36,78 @@
 </script>
 
 <Modal close={() => router.abort('/')}>
+  <form on:submit|preventDefault={submit}>
+    {#if message}
+      <slot name="message">
+        <div class="error" id="message">{message}</div>
+      </slot>
+    {/if}
 
-<form on:submit|preventDefault={submit}>
-  {#if message}
-    <slot name="message">
-      <div class="error" id="message">{message}</div>
+    <slot name="inputs">
+      <label for="username">
+        Username
+        <input
+          aria-label="username"
+          aria-required="true"
+          maxlength="75"
+          size="32"
+          autocorrect="off"
+          autocapitalize="off"
+          autocomplete="username"
+          id="username"
+          type="text"
+          placeholder="Username"
+          name="username"
+          required
+          disabled={active}
+          bind:value={username} />
+      </label>
+      <label for="new-password">
+        New Password
+        <input
+          aria-label="new password"
+          aria-required="true"
+          size="32"
+          autocorrect="off"
+          autocapitalize="off"
+          autocomplete="new-password"
+          id="new-password"
+          type="password"
+          placeholder="New Password"
+          name="new-password"
+          required
+          disabled={active}
+          bind:value={newPassword} />
+      </label>
+      <label for="repeated-new-password">
+        Repeat New Password
+        <input
+          aria-label="repeated new password"
+          aria-required="true"
+          size="32"
+          autocorrect="off"
+          autocapitalize="off"
+          autocomplete="repeated-new-password"
+          id="repeated-new-password"
+          type="password"
+          placeholder="New Password"
+          name="repeated-new-password"
+          required
+          disabled={active}
+          bind:value={repeatedNewPassword} />
+      </label>
     </slot>
-  {/if}
 
-  <slot name="inputs">
-    <label for="username">
-      Username
-      <input
-        aria-label="username"
-        aria-required="true"
-        maxlength="75"
-        size="32"
-        autocorrect="off"
-        autocapitalize="off"
-        autocomplete="username"
-        id="username"
-        type="text"
-        placeholder="Username"
-        name="username"
-        required
-        disabled={active}
-        bind:value={username} />
-    </label>
-    <label for="new-password">
-      New Password
-      <input
-        aria-label="new password"
-        aria-required="true"
-        size="32"
-        autocorrect="off"
-        autocapitalize="off"
-        autocomplete="new-password"
-        id="new-password"
-        type="password"
-        placeholder="New Password"
-        name="new-password"
-        required
-        disabled={active}
-        bind:value={newPassword} />
-    </label>
-    <label for="repeated-new-password">
-      Repeat New Password
-      <input
-        aria-label="repeated new password"
-        aria-required="true"
-        size="32"
-        autocorrect="off"
-        autocapitalize="off"
-        autocomplete="repeated-new-password"
-        id="repeated-new-password"
-        type="password"
-        placeholder="New Password"
-        name="repeated-new-password"
-        required
-        disabled={active}
-        bind:value={repeatedNewPassword} />
-    </label>
-  </slot>
-
-  <slot name="submit">
-    <button id="submit" type="submit"
-      disabled={!username || !newPassword || newPassword !== repeatedNewPassword}>
-      Change Password
-      {#if active}
-        <div class="spinner" />
-      {/if}
-    </button>
-  </slot>
-</form>
-
+    <slot name="submit">
+      <button
+        id="submit"
+        type="submit"
+        disabled={!username || !newPassword || newPassword !== repeatedNewPassword}>
+        Change Password
+        {#if active}
+          <div class="spinner" />
+        {/if}
+      </button>
+    </slot>
+  </form>
 </Modal>
