4162b150ef1a42d550a7abd5dc30ec2b74266199
Markus Felten <Markus.Felten@gmx.de>
2020-12-03 18:53:41 +0100

    fix: check fetch response

--- src/util.mjs
+++ src/util.mjs
@@ -1,22 +1,26 @@
 import api from "consts:api";
 
 export async function* fetchIterator(path, factory, session, transition) {
-  const res = await fetch(api + path, {
+  const response = await fetch(api + path, {
     headers: {
       ...session.authorizationHeader
     }
   });
 
-  for (const u of await res.json()) {
+  if (response.ok) {
+    for (const u of await response.json()) {
       yield new factory(u);
+    }
   }
 }
 
 export async function fetchObject(path, factory, session) {
-  const res = await fetch(api + path, {
+  const response = await fetch(api + path, {
     headers: {
       ...session.authorizationHeader
     }
   });
-  return new factory(await res.json());
+  if (response.ok) {
+    return new factory(await response.json());
+  }
 }
